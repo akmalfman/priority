e99002f98226ddba38f645731a3d91b582260ec7
akmalfman <akmalfsalman2@gmail.com>
2023-12-15 22:29:53 +0700

    fix main activity

--- app/src/main/java/com/example/priority/MainActivity.kt
+++ app/src/main/java/com/example/priority/MainActivity.kt
@@ -6,7 +6,6 @@ import com.example.priority.databinding.ActivityMainBinding
 import androidx.core.view.MenuItemCompat
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
-import com.example.priority.databinding.ActivityProfileBinding
 import me.ibrahimsn.lib.SmoothBottomBar
 
 class MainActivity : AppCompatActivity(),  OnSmoothBottomBarItemSelectedListener {
