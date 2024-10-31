1ce304ef6ea24b1c90fbe00063493c0831487b8b
atjnr <astajuniar14@gmail.com>
2024-10-31 13:14:34 +0700

    update dashboard

--- app/src/main/java/com/example/priority/view/main/DashboardViewModel.kt
+++ app/src/main/java/com/example/priority/view/main/DashboardViewModel.kt
@@ -1,24 +1,12 @@
 package com.example.priority.view.main
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.example.priority.data.ResultState
 import com.example.priority.data.api.ApiConfigg
 import com.example.priority.data.response.AqiResponse
-import com.example.priority.data.response.FileUploadResponse
 import com.google.gson.Gson
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.HttpException
-import retrofit2.Response
-import java.io.File
 
 class DashboardViewModel : ViewModel() {
 
